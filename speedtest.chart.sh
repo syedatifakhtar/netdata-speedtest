dfde8b54d4299d8608cee7af8599bee572351114
Atif Akhtar <syedatifakhtar@gmail.com>
2021-05-17 23:22:18 +0530

    Use ookla speed test cli

--- speedtest.chart.sh
+++ speedtest.chart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Netdata charts.d collector for fast.com internet speed test.
-# Requires installed speedtest.com cli: `pip install speedtest-cli`
-speedtest_update_every=60
+# Requires installed speedtest.com cli: `sudo apt-get install speedtest`
+speedtest_update_every=600
 speedtest_priority=100
 
 speedtest_check() {
-  require_cmd speedtest-cli || return 1
+  require_cmd speedtest || return 1
   return 0
 }
 
@@ -15,8 +15,8 @@ speedtest_create() {
 	# create a chart with 2 dimensions
 	cat <<EOF
 CHART system.connectionspeed '' "System Connection Speed" "Mbps" "connection speed" system.connectionspeed line $((speedtest_priority + 1)) $speedtest_update_every
-DIMENSION down 'Down' absolute 1 1000000
-DIMENSION up 'Up' absolute 1 1000000
+DIMENSION down 'Down' absolute 1 1000
+DIMENSION up 'Up' absolute 1 1000
 EOF
 
 	return 0
@@ -27,15 +27,19 @@ speedtest_update() {
 	# for each dimension
 	# remember: KEEP IT SIMPLE AND SHORT
   # Get the up and down speed. Parse them into separate values, and drop the Mbps.
-  speedtest_output=$(speedtest-cli --single --csv)
-  down=$(echo $speedtest_output | cut -d ',' -f 7 | cut -d '.' -f 1)
-  up=$(echo $speedtest_output | cut -d ',' -f 8 | cut -d '.' -f 1)
+  speedtest_output=$(speedtest -f json --accept-license --accept-gdpr)
+  down=$(echo $speedtest_output | jq ."download.bandwidth")
+  up=$(echo $speedtest_output | jq ."upload.bandwidth")
+  bytesToMbits=0.000008
+
+  down_mbytes=$( echo "$down * $bytesToMbits" | bc)
+  up_mbytes=$( echo "$up * $bytesToMbits" | bc)
 
 	# write the result of the work.
 	cat <<VALUESEOF
 BEGIN system.connectionspeed
-SET down = $down
-SET up = $up
+SET down = $down_mbytes
+SET up = $up_mbytes
 END
 VALUESEOF
 
